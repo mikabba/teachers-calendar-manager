939b63b0869f2b4512230ba975cda95f224f8d78
Michele Abbaticchio <121615241+mikabba@users.noreply.github.com>
2026-05-18 22:56:39 +0200

    Create users.example.php

---
+++ examples/users.example.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    array(
+        'username' => 'admin',
+        'displayName' => 'Demo Admin',
+        'role' => 'admin',
+        'passwordHash' => password_hash('admin12345', PASSWORD_DEFAULT)
+    )
+);
